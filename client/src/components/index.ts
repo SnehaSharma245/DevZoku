73336be90641fc9b705551c88733252ea4bad3e6
Sneha Sharma <snehav2109@gmail.com>
2025-07-06 18:00:25 +0530

    common export for shadcn components

---
+++ client/src/components/index.ts
@@ -0,0 +1,39 @@
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+
+import { Switch } from "./ui/switch";
+import { Card, CardContent } from "./ui/card";
+import { Label } from "./ui/label";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "./ui/form";
+import { useForm } from "react-hook-form";
+import { useFieldArray } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+export {
+  Input,
+  Textarea,
+  Button,
+  Switch,
+  Card,
+  CardContent,
+  Label,
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  useForm,
+  useFieldArray,
+  zodResolver,
+  z,
+};
