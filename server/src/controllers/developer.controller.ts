81f6f465460403ba8c11f0a39bd559abb83648f8
Sneha Sharma <snehav2109@gmail.com>
2025-07-09 09:55:39 +0530

    add team management routes and implement related controllers

--- server/src/controllers/developer.controller.ts
+++ server/src/controllers/developer.controller.ts
@@ -1,4 +1,4 @@
-import { eq } from "drizzle-orm";
+import { eq, desc, and } from "drizzle-orm";
 import { db } from "../db";
 import { developers } from "../db/schema/developer.schema";
 import { ApiResponse } from "../utils/ApiResponse";
@@ -6,7 +6,8 @@ import { completeDeveloperProfileSchema } from "../zod-schema/developer.schema";
 import type { Request, Response } from "express";
 import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
-import { teams } from "../db/schema/team.schema";
+import { teamMembers, teams } from "../db/schema/team.schema";
+import { users } from "../db/schema/user.schema";
 
 // Controller to handle completing a developer's profile
 const completeDeveloperProfile = asyncHandler(
@@ -157,6 +158,7 @@ const createTeam = asyncHandler(async (req: Request, res: Response) => {
   }
 });
 
+// Controller to check if a team name is unique
 const checkTeamNameUnique = asyncHandler(
   async (req: Request, res: Response) => {
     const { teamName } = req.query;
@@ -192,4 +194,131 @@ const checkTeamNameUnique = asyncHandler(
   }
 );
 
-export { completeDeveloperProfile, createTeam, checkTeamNameUnique };
+// controller to get joined teams
+const getJoinedTeams = asyncHandler(async (req: Request, res: Response) => {
+  const { user } = req;
+
+  if (!user) {
+    throw new ApiError(401, "User not authenticated");
+  }
+
+  const joinedTeams = await db
+    .select({ team: teams })
+    .from(teamMembers)
+    .innerJoin(teams, eq(teams.id, teamMembers.teamId))
+    .where(eq(teamMembers.userId, user.id))
+    .execute();
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, joinedTeams, "Joined teams fetched successfully")
+    );
+});
+
+// controller to view all teams and a particular team by ID
+const viewAllTeams = asyncHandler(async (req: Request, res: Response) => {
+  const { user } = req;
+  const { id } = req.params;
+
+  if (!user) {
+    throw new ApiError(401, "User not authenticated");
+  }
+
+  const allTeams = await db
+    .select({ team: teams, captain: users })
+    .from(teams)
+    .orderBy(desc(teams.createdAt))
+    .innerJoin(users, eq(teams.captainId, users.id))
+    .execute();
+
+  const particularTeam = allTeams.find((team) => team.team.id === id);
+
+  if (id && !particularTeam) {
+    throw new ApiError(404, "Team not found");
+  }
+
+  if (id && particularTeam) {
+    console.log("Particular Team:", particularTeam);
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          particularTeam,
+          "Particular team fetched successfully"
+        )
+      );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, allTeams, "All teams fetched successfully"));
+});
+
+const joinInvitation = asyncHandler(async (req: Request, res: Response) => {
+  const { user } = req;
+  const { teamId } = req.body;
+
+  if (!user) {
+    throw new ApiError(401, "User not authenticated");
+  }
+
+  if (user.role !== "developer") {
+    throw new ApiError(403, "Access denied. Only developers can join teams.");
+  }
+
+  // check if the team exists
+  const existingTeam = await db
+    .select()
+    .from(teams)
+    .where(eq(teams.id, teamId))
+    .limit(1)
+    .execute();
+
+  if (existingTeam.length === 0) {
+    throw new ApiError(404, "Team not found");
+  }
+
+  //check if the team is accepting invites
+  if (!existingTeam[0]?.isAcceptingInvites) {
+    throw new ApiError(400, "This team is not accepting invites at the moment");
+  }
+
+  //check if the user is already a member of the team
+  const existingMember = await db
+    .select()
+    .from(teamMembers)
+    .where(and(eq(teamMembers.userId, user.id), eq(teamMembers.teamId, teamId)))
+    .limit(1)
+    .execute();
+
+  if (existingMember.length > 0) {
+    throw new ApiError(400, "You are already a member of this team");
+  }
+
+  // Add the user to the team
+  const insertionResponse = await db
+    .insert(teamMembers)
+    .values({ teamId: teamId, userId: user.id })
+    .returning();
+
+  if (insertionResponse.length === 0) {
+    throw new ApiError(500, "Failed to join the team");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, insertionResponse[0], "Successfully joined the team")
+    );
+});
+
+export {
+  completeDeveloperProfile,
+  createTeam,
+  checkTeamNameUnique,
+  getJoinedTeams,
+  viewAllTeams,
+  joinInvitation,
+};
--- server/src/routes/developer.route.ts
+++ server/src/routes/developer.route.ts
@@ -3,6 +3,9 @@ import {
   checkTeamNameUnique,
   completeDeveloperProfile,
   createTeam,
+  getJoinedTeams,
+  viewAllTeams,
+  joinInvitation,
 } from "../controllers/developer.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
@@ -12,4 +15,8 @@ const router = Router();
 router.post("/complete-profile", verifyJWT, completeDeveloperProfile);
 router.post("/create-team", verifyJWT, createTeam);
 router.get("/check-teamName-unique", verifyJWT, checkTeamNameUnique);
+router.get("/joined-teams", verifyJWT, getJoinedTeams);
+router.get("/view-all-teams", verifyJWT, viewAllTeams);
+router.get("/view-all-teams/:id", verifyJWT, viewAllTeams);
+router.post("/join-invitation", verifyJWT, joinInvitation);
 export default router;
