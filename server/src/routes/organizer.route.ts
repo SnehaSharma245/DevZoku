67cbe8b53703f2b5b00ced14bf4f56c02ddca204
Sneha Sharma <snehav2109@gmail.com>
2025-07-10 21:17:14 +0530

    feat: add create-hackathon route to organizer routes

--- server/src/routes/organizer.route.ts
+++ server/src/routes/organizer.route.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { completeOrganizerProfile } from "../controllers/organizer.controller";
+import {
+  completeOrganizerProfile,
+  createHackathon,
+} from "../controllers/organizer.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 // Protected route for completing organizer profile
 router.post("/complete-profile", verifyJWT, completeOrganizerProfile);
+router.post("/create-hackathon", verifyJWT, createHackathon);
 
 export default router;
