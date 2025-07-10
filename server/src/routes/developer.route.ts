b4897742053b12aa5ded5e8d856fc05c298b4b8c
Sneha Sharma <snehav2109@gmail.com>
2025-07-10 17:58:34 +0530

    feat: enhance developer routes with invitation and notification handling

--- server/src/routes/developer.route.ts
+++ server/src/routes/developer.route.ts
@@ -5,7 +5,10 @@ import {
   createTeam,
   getJoinedTeams,
   viewAllTeams,
-  joinInvitation,
+  sendInvitation,
+  fetchPendingInvitesAndAcceptThem,
+  fetchSentInvitations,
+  notificationHandling,
 } from "../controllers/developer.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
@@ -18,5 +21,13 @@ router.get("/check-teamName-unique", verifyJWT, checkTeamNameUnique);
 router.get("/joined-teams", verifyJWT, getJoinedTeams);
 router.get("/view-all-teams", verifyJWT, viewAllTeams);
 router.get("/view-all-teams/:id", verifyJWT, viewAllTeams);
-router.post("/join-invitation", verifyJWT, joinInvitation);
+router.post("/send-invitation", verifyJWT, sendInvitation);
+router.post(
+  "/fetch-invites-and-accept/:teamId",
+  verifyJWT,
+  fetchPendingInvitesAndAcceptThem
+);
+router.get("/sent-invitations", verifyJWT, fetchSentInvitations);
 export default router;
+router.get("/notifications", verifyJWT, notificationHandling);
+router.delete("/notifications/:id", verifyJWT, notificationHandling);
