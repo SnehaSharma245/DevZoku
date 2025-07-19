885abe3f3c285a8124d7381c0177c92ccfb19f59
Sneha Sharma <snehav2109@gmail.com>
2025-07-19 12:12:43 +0530

    feat: add Hackathon and Phases interfaces

---
+++ client/src/types/hackathon.types.ts
@@ -0,0 +1,37 @@
+export interface Hackathon {
+  id: string;
+  title: string;
+  description?: string;
+  registrationStart?: string;
+  registrationEnd?: string;
+  createdBy?: string;
+  organizationName?: string;
+  startTime: string;
+  endTime: string;
+  participants?: string[];
+  status?:
+    | "upcoming"
+    | "ongoing"
+    | "completed"
+    | "Registration in Progress"
+    | "Registration ended"
+    | "unknown";
+  tags: string[];
+  poster?: string;
+  minTeamSize?: number;
+  maxTeamSize?: number;
+  location?: string;
+  mode?: "online" | "offline";
+  phases?: Phases[];
+  dateCompleted?: string; // archived field
+}
+
+export interface Phases {
+  id: string;
+  hackathonId: string;
+  title: string;
+  description: string;
+  startTime: string;
+  endTime: string;
+  order: number;
+}
