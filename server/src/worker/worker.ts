d3b1f5aac03f6c6060773826e627be1d58ca8a16
Sneha Sharma <snehav2109@gmail.com>
2025-07-23 11:23:41 +0530

    feat: update hackathon email worker to handle

--- server/src/worker/worker.ts
+++ server/src/worker/worker.ts
@@ -2,9 +2,13 @@ import { Worker } from "bullmq";
 import { teamRegToHackathonTemplate } from "../templates/teamRegToHackathon";
 import transporter from "../utils/nodemailerUtility";
 import { connection } from "../queues/queue";
+import { buildUserInteractionText } from "../utils/vector/buildUserIteraction";
+import { initialiseVectorStore } from "../lib/vectorStore";
+import { Document } from "@langchain/core/documents";
+import { hackathonResultAnnouncementTemplate } from "../templates/HackathonWinnerAnnouncement";
 
 const hackathonTeamEmailWorker = new Worker(
-  "team-hackathon-registration-emails",
+  "hackathon-emails",
   async (job) => {
     const {
       email,
@@ -15,29 +19,54 @@ const hackathonTeamEmailWorker = new Worker(
       hackathonEndDate,
       organizationName,
       organizationEmail,
+      type,
+      position,
+      captainName,
     } = job.data;
 
-    const message = teamRegToHackathonTemplate({
-      memberName,
-      teamName,
-      hackathonName,
-      hackathonStartDate,
-      hackathonEndDate,
-      organizationName,
-      organizationEmail,
-    });
+    if (type === "team-registration") {
+      const message = teamRegToHackathonTemplate({
+        memberName,
+        teamName,
+        hackathonName,
+        hackathonStartDate,
+        hackathonEndDate,
+        organizationName,
+        organizationEmail,
+      });
 
-    await transporter(
-      organizationEmail,
-      organizationName || "DevZoku",
-      email,
-      `Team Registration Confirmation for ${teamName} at ${hackathonName}`,
-      message
-    );
+      await transporter(
+        organizationEmail,
+        organizationName || "DevZoku",
+        email,
+        `Team Registration Confirmation for ${teamName} at ${hackathonName}`,
+        message
+      );
+    }
+
+    if (type === "hackathon-result-announcement") {
+      const message = hackathonResultAnnouncementTemplate({
+        captainName,
+        teamName,
+        hackathonName,
+        organizationName,
+        organizationEmail,
+        position,
+      });
+
+      await transporter(
+        organizationEmail,
+        organizationName || "DevZoku",
+        email,
+        `Hackathon Results for ${teamName} at ${hackathonName}`,
+        message
+      );
+    }
   },
   {
     concurrency: 100,
     connection: connection,
   }
 );
+
 export { hackathonTeamEmailWorker };
