526dda5ee2f5e8d840fe7f79c024c11e13adb30a
Sneha Sharma <snehav2109@gmail.com>
2025-08-05 19:05:45 +0530

    enhance socket connection configuration

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -5,6 +5,7 @@ const SOCKET_URI =
   process.env.NEXT_PUBLIC_SOCKET_URI || "http://localhost:8000";
 export const socket = io(SOCKET_URI, {
   path: "/socket/",
+  withCredentials: true,
   transports: ["websocket", "polling"],
 });
 socket.on("connect", () => {
