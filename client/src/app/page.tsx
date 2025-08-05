2e9d40e525e441e1c75cedc1519f21bb78455123
Sneha Sharma <snehav2109@gmail.com>
2025-08-05 17:48:29 +0530

    feat: add toast notification for error messages

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,19 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 
 import HeroSection from "@/components/landing-page-components/HeroSection";
+import { useSearchParams } from "next/navigation";
+import { toast } from "sonner";
 
 const Home = () => {
+  const searchParams = useSearchParams();
+  const toastMessage = searchParams.get("toast");
+
+  useEffect(() => {
+    if (toastMessage) {
+      toast.error(toastMessage);
+    }
+  }, [toastMessage]);
   return (
     <>
       <HeroSection />
